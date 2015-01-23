c92ef16a5e965a74e8b6fb08323524dd3cfd1285
billwiliams <wnwillyndirangu@gmail.com>
2015-01-23 18:52:25 +0300

    Added the Least Squares Function

--- src/NonLinearRegression.java
+++ src/NonLinearRegression.java
@@ -20,42 +20,45 @@ public class NonLinearRegression {
         //Filename containing the data
         String csvFilename = "D:\\projects\\Non_linear_regression\\ArcTan\\ArcTan_Data.csv";
         //Reading the csv file but ignoring the first column since it contains headings the result is stored in a list
-        CSVReader csvReader = new CSVReader(new FileReader(csvFilename),',', '\'', 1);
+        CSVReader csvReader = new CSVReader(new FileReader(csvFilename), ',', '\'', 1);
         List content = csvReader.readAll();
         //variable to hold each row of the List while iterating through it
         String[] row = null;
         //counter used to populate the variables with data from the csv file
-        int counter=0;
+        int counter = 0;
         //initializing the variables to hold data in the csv file
-        double X_m []=new double[content.size()];
-        double Y_m []=new double[content.size()];
-        String Date[]=new String[content.size()];
-        double Speed[]= new double[content.size()];
-        double Course_degs[]= new double[content.size()];
-        double Bearing_degs[]= new double[content.size()];
+        double X_m[] = new double[content.size()];
+        double Y_m[] = new double[content.size()];
+        String Date[] = new String[content.size()];
+        double Speed[] = new double[content.size()];
+        double Course_degs[] = new double[content.size()];
+        double Bearing_degs[] = new double[content.size()];
+        double Xo[] ;
+        double Yo[];
+        double SquaredErrors;
 
         for (Object object : content) {
             row = (String[]) object;
             /*parsing data from the list to the variables */
-            Date[counter]=row[0].toString();
-            X_m[counter]=(Double.parseDouble(row[1].toString()));
-            Y_m[counter]=(Double.parseDouble(row[2].toString()));
-            Course_degs[counter]=(Double.parseDouble(row[3].toString()));
-            Speed[counter]=(Double.parseDouble(row[4].toString()));
-            Bearing_degs[counter]=(Double.parseDouble(row[5].toString()));
+            Date[counter] = row[0].toString();
+            X_m[counter] = (Double.parseDouble(row[1].toString()));
+            Y_m[counter] = (Double.parseDouble(row[2].toString()));
+            Course_degs[counter] = (Double.parseDouble(row[3].toString()));
+            Speed[counter] = (Double.parseDouble(row[4].toString()));
+            Bearing_degs[counter] = (Double.parseDouble(row[5].toString()));
             counter++;
-               }
+        }
 
         csvReader.close();
-        //for verification of obtained variables
-        for (int i=0;i<content.size();i++) {
-            System.out.println(Bearing_degs[i]);
-
-
+        //obtaining the values of Yo' and Xo'
+        Xo = MathematicalComputations.ConvertCourseX_m(Course_degs, Speed);
+        Yo = MathematicalComputations.ConvertCourseY_m(Course_degs, Speed);
+        SquaredErrors=MathematicalComputations.LeastSquares(Yo,Xo);
+       for (counter=0; counter < content.size(); counter++) {
+            System.out.println(SquaredErrors);
         }
+
+
     }
-
-
-
     }
 
